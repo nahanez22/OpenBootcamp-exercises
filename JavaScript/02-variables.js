c49879d47aeeb66bc9a93091450d98c47eeb5de4
Nahum Añez <nahumatencio@gmail.com>
2022-11-08 22:31:44 -0500

    Declaring variables

---
+++ JavaScript/02-variables.js
@@ -0,0 +1,16 @@
+const quienSoy = new Array(
+  "Nahum Añez",
+  33,
+  true,
+  (fechaNacimiento = new Date("may 22 1989"))
+);
+console.log(quienSoy);
+
+const doñaBarbara = {
+  titulo: "Doña Barbara",
+  autor: "Romulo Gallegos",
+  fecha: 1929,
+  url: "https://www.guao.org/sites/default/files/biblioteca/Dona%20Barbara%20%20Romulo%20Gallegos.pdf",
+};
+
+console.log(doñaBarbara);
